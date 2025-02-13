7a75ae1ed0e85b977be57da7d3a4293dc4611419
Jack Arthur Harrhy <me@jackharrhy.com>
2025-02-13 19:45:29 -0330

    routing that sparks joy

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import React, { useState, useRef, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { motion, AnimatePresence } from "motion/react";
-import music from "/assets/music.mp3";
-import { IconButton } from "@mui/material";
-import MusicNoteIcon from "@mui/icons-material/MusicNote";
-import MusicOffIcon from "@mui/icons-material/MusicOff";
 import Home from "./Home";
 import Game from "./Game";
+import GameOver from "./GameOver";
 
 function App() {
-  const [showStartScreen, setShowStartScreen] = useState(true); 
-
-  const handleStart = () => {
-    setShowStartScreen(false);
-  };
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home onStart={handleStart} showStartScreen={showStartScreen} />} />
-        <Route path="/game" element={<Game />} />
-      </Routes>
-    </Router>
+    <div className="app-container">
+      <img
+        src="/assets/background.png"
+        alt="Background"
+        className="home-background"
+      />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/game-over" element={<GameOver />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
