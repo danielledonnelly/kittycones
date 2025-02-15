8a704dc8d9ef67d34595160a632b580e1ef13b22
Danielle Donnelly <145680391+danielledonnelly@users.noreply.github.com>
2025-02-15 16:45:58 -0330

    css

--- src/About.jsx
+++ src/About.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <div className="end screen end-screen">
-      <h1 className="end screen-title">About Kitty Cones</h1>
-      <p className="end screen-text">
+      <h1 className="about screen-title">About Kitty Cones</h1>
+      <p className="about screen-text">
         Kitty Cones is a fun and fast-paced game where you serve ice cream to 
         hungry kitties! Click on the ingredients and match their orders before 
         time runs out. The faster you serve, the more coins you earn!
       </p>
-      <p className="end screen-text">
+      <p className="about screen-text">
         Challenge yourself to beat your high score and climb the leaderboard!
       </p>
       <div className="button-group">
